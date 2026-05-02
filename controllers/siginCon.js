7afa5ac902b6761e990ccaf66fecb6b2efc0c6a3
nitish100000100-found <nkdabur7@gmail.com>
2026-05-02 15:27:10 +0530

    Update siginCon.js

--- controllers/siginCon.js
+++ controllers/siginCon.js
@@ -3,44 +3,30 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 async function signin(req, res, next) {
-  
-  
   const { username, password } = req.body;
-  
 
   try {
-    
     const user = await User.findOne({ username });
-
     if (!user) {
-      return res.status(200).json({"message":"This username doesn't exist"});
+      return res.status(200).json({ success: false, message: "This username doesn't exist" });
     }
-
 
     const isMatch = await bcrypt.compare(password, user.password);
-
     if (!isMatch) {
-      return res.status(200).json({"message":"Wrong Password"});
+      return res.status(200).json({ success: false, message: "Wrong Password" });
     }
-
 
     const token = jwt.sign(
       { id: user._id, loggedIn: true, username },
-     process.env.JWT_SECRET,
-      {
-        expiresIn: "7d",
-      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
     );
-res.cookie("token", token, {
-  httpOnly: true,
-  secure: true,        
-  sameSite: "none",   
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-});
-    res.status(200).json({ "message": "OK" });
+
+    return res.status(200).json({ success: true, token: token, username: username });
+
   } catch (err) {
     console.log(err);
-    return res.status(200).json({"message":"Databse Error"});
+    return res.status(200).json({ success: false, message: "Database Error" });
   }
 }
 
